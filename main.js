5fd906bccf46c18fc2747f1ad1a9d8557fd7dd90
metodomaker <viniciussouza.on@gmail.com>
2021-08-22 18:44:44 -0300

    ok

--- main.js
+++ main.js
@@ -82,3 +82,10 @@ function upvoteProduct(id) {
 	
 }
 
+function Mudarestado(el) {
+	var display = document.getElementById(el).style.display;
+	if(display == "none")
+		document.getElementById(el).style.display = 'block';
+	else
+		document.getElementById(el).style.display = 'none';
+}
